714df04a0d589df33e0ac1c9311774c381110e3b
kishore7483 <112850619+kishore7483@users.noreply.github.com>
2022-10-10 10:24:19 +0530

    Add files via upload

---
+++ program_42,c++...cpp
@@ -0,0 +1,34 @@
+include<iostream>
+using namespace std;
+class college
+{
+  public:
+    college()
+    { cout << "a: "<< 10 << endl;}
+};
+
+class sem: public college
+{
+  public:
+    sem()
+    { cout<< "b: " << 15 << endl;}
+};
+
+class marks: public college
+{
+  public:
+    marks()
+    { cout << "c: "<< 20 <<endl;}
+};   
+
+class extracurricular: public marks, public sem
+{
+  public:
+    extracurricular()
+    { cout << "D: "<< 25<<endl;}
+};
+
+int main()
+{
+  extracurricular obj2;
+}
