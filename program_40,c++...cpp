74600f132cdb72c192d064db5eec77a6c5352c53
kishore7483 <112850619+kishore7483@users.noreply.github.com>
2022-10-10 10:07:36 +0530

    Add files via upload

---
+++ program_40,c++...cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int a[2][2] = {{1,2},{2,3}};
+int b[2][2] = {{1,2},{2,3}};
+int c[2][2];
+void add ()
+{
+  for (int i = 0 ; i < 2 ; i++)
+  {
+    for (int j = 0 ; j < 2 ; j++)
+    {
+      c[i][j] = a[i][j] + b[i][j];
+    }
+  }
+}
+int main()
+{
+  add();
+  for (int i = 0 ; i < 2 ; i++)
+  {
+    for (int j = 0 ; j < 2 ; j++)
+    {
+      cout << c[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
